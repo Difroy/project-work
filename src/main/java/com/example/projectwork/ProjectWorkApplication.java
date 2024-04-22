be9e480d69b4e07a832fbb949332ee1802a335af
Difroy <fromomi18@gmail.com>
2024-04-22 17:18:58 +0200

    Initial commit

---
+++ src/main/java/com/example/projectwork/ProjectWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWorkApplication.class, args);
+	}
+
+}
