732cd4a7f188eddfc1719a424f6b5a1ae9503f32
SimoneCantella <simo.cantella@gmail.com>
2024-04-22 20:01:55 +0200

    Creazione controller e rinominazione progetto
    Yolo

---
+++ src/main/java/projectWork/Controller/Controller.java
@@ -0,0 +1,5 @@
+package projectWork.Controller;
+
+public class Controller {
+
+}
--- src/main/java/projectWork/ProjectWorkApplication.java
+++ src/main/java/projectWork/ProjectWorkApplication.java
@@ -1,4 +1,4 @@
-package com.example.projectwork;
+package projectWork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
