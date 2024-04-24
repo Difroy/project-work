57e5966ed5b360c4c4aa9c6596a9daec992d6025
SimoneCantella <simo.cantella@gmail.com>
2024-04-24 12:08:38 +0200

    mi son dimenticato i getters e setters in Profilo.java, eccoli

--- src/main/java/projectWork/Model/Profilo.java
+++ src/main/java/projectWork/Model/Profilo.java
@@ -22,6 +22,54 @@ public class Profilo {
 	@Column
 	private String cognome;
 	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public LocalDate getDataDiNascita() {
+		return dataDiNascita;
+	}
+
+	public void setDataDiNascita(LocalDate dataDiNascita) {
+		this.dataDiNascita = dataDiNascita;
+	}
+
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
 	@Column
 	private LocalDate dataDiNascita;
 	
