0b8066e499f49719c8e01b056da97a00a0f88004
Difroy <fromomi18@gmail.com>
2024-04-26 10:51:11 +0200

    Implementazione navbar

--- src/main/java/projectWork/Controller/IndexController.java
+++ src/main/java/projectWork/Controller/IndexController.java
@@ -1,5 +1,14 @@
 package projectWork.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class IndexController {
 
+	//localhost:8080
+	@Controller
+	@RequestMapping("/")
+	public class IndexController{
+		
+	}
 }
