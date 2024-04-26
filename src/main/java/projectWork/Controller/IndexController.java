36756c1be0e443aa5cda825aae45e57b7e50bffb
Difroy <fromomi18@gmail.com>
2024-04-26 14:43:12 +0200

    Creazione IndexTest e primo completamento dell'IndexController

--- src/main/java/projectWork/Controller/IndexController.java
+++ src/main/java/projectWork/Controller/IndexController.java
@@ -1,16 +1,32 @@
 package projectWork.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpSession;
+import projectWork.Model.Categoria;
+import projectWork.Service.CategoriaService;
 
 //localhost:8080
 @Controller
 @RequestMapping("/")
 public class IndexController {
-@Autowired
+	@Autowired
+	private CategoriaService categoriaService;
 
-	
-	
-	
+	@GetMapping
+	public String getPage(Model model, HttpSession session) {
+
+		List<Categoria> categoria = categoriaService.getCategorie();
+		model.addAttribute("categoria", categoria);
+
+		return "index";
+
+	}
+
 }
