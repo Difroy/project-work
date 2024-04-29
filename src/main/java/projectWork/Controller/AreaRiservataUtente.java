6f1d4b9e2c39cdbd73fc4c14e44034f8825f38ca
Difroy <fromomi18@gmail.com>
2024-04-29 10:17:15 +0200

    Update
    A

--- src/main/java/projectWork/Controller/AreaRiservataUtente.java
+++ src/main/java/projectWork/Controller/AreaRiservataUtente.java
@@ -1,5 +1,14 @@
 package projectWork.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/areariservata")
 public class AreaRiservataUtente {
 
+@GetMapping
+public String getPage (Model model)
+	
 }
