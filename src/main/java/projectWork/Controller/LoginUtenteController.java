f926d291e00e15c376050daf4ee06aa011e5ec7f
SimoneCantella <simo.cantella@gmail.com>
2024-04-29 09:40:36 +0200

    fatto LoginUtenteController

--- src/main/java/projectWork/Controller/LoginUtenteController.java
+++ src/main/java/projectWork/Controller/LoginUtenteController.java
@@ -1,5 +1,43 @@
 package projectWork.Controller;
 
-public class LoginUtenteController {
+import jakarta.servlet.http.HttpSession;
+import projectWork.Service.ProfiloService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/loginutente")
+public class LoginUtenteController
+{
+    @Autowired
+    private ProfiloService profiloService;
+    
+    @GetMapping
+   public String getPage(HttpSession session, @RequestParam(name = "error", required = false) String error, Model model)
+    {
+    	if(session.getAttribute("profilo") != null)
+    		return "redirect:/riservatautente";
+    	model.addAttribute("error", error);
+        return "loginutente";
+    }
+
+    @PostMapping
+    public String formManager(
+            @RequestParam("username") String username,
+            @RequestParam("password") String password,
+            HttpSession session
+    )
+    {
+    	
+    	if(!profiloService.loginProfilo(username, password, session))
+    		return "redirect:/loginutente?error";
+        return "redirect:/riservatautente";
+    }
+    	
 }
