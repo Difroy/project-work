3033a01b9157a5b6874412c999cf606787b3f13d
SimoneCantella <simo.cantella@gmail.com>
2024-05-03 14:19:40 +0200

    prova maggiorenne

--- src/main/java/projectWork/controller/RegistrazioneUtenteController.java
+++ src/main/java/projectWork/controller/RegistrazioneUtenteController.java
@@ -1,5 +1,8 @@
 package projectWork.controller;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,6 +26,8 @@ public class RegistrazioneUtenteController {
 	@Autowired
 	public UtenteService utenteService;
 	
+	
+	
 	@GetMapping
 	public String getPage (Model model) {
 		Utente utente = new Utente ();
@@ -44,5 +49,34 @@ public class RegistrazioneUtenteController {
 	    session.setAttribute("utente", utente); // Aggiungi l'utente alla sessione
 	    return "redirect:/areariservata"; // Reindirizza alla pagina dell'area riservata
 	}
+	
+	//prova maggiorenne
+	@GetMapping("/registrazioneutente")
+    public String mostraFormRegistrazione(Model model) {
+		Utente utente = new Utente ();
+        model.addAttribute("utente", utente);
+        return "registrazioneutente";
+    }
+
+    @PostMapping("/registrazioneutente")
+    public String processaFormRegistrazione(@ModelAttribute("utente") Utente utente, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "registrazioneutente";
+        }
+
+        LocalDate dataDiNascita = utente.getProfilo().getDataDiNascita();
+        if (dataDiNascita != null) {
+            LocalDate oggi = LocalDate.now();
+            Period period = Period.between(dataDiNascita, oggi);
+            if (period.getYears() < 18) {
+                model.addAttribute("erroreMaggiorenne", true);
+                return "registrazioneutente";
+            }
+        }
+
+        // Altre operazioni di registrazione
+
+        return "redirect:/login"; 
+    }
 }
 
