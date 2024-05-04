adce53c532fd51e640b25499bb1f2e979568a1cf
Moemie94 <167971463+Moemie94@users.noreply.github.com>
2024-05-04 21:14:39 +0200

    Revert "update"
    This reverts commit eca35d6c1554bdd53d1e6554a925bbb79fd3db2a.

--- src/main/java/projectWork/service/UtenteServiceImpl.java
+++ src/main/java/projectWork/service/UtenteServiceImpl.java
@@ -2,7 +2,7 @@ package projectWork.service;
 
 import jakarta.servlet.http.HttpSession;
 
-
+import org.hibernate.boot.jaxb.JaxbLogger_.logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
