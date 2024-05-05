b8246f91032b406412167ceb69d3652135f1eacc
Difroy <fromomi18@gmail.com>
2024-05-05 13:16:05 +0200

    Update Bug registrazione

--- src/main/java/projectWork/controller/RegistrazioneUtenteController.java
+++ src/main/java/projectWork/controller/RegistrazioneUtenteController.java
@@ -40,28 +40,8 @@ public class RegistrazioneUtenteController {
 	}
 	//prova maggiorenne
 	
+
 	
-	  @PostMapping public String
-	  processaFormRegistrazione(@ModelAttribute("utente") Utente utente,
-	  BindingResult result, Model model) { if (result.hasErrors()) { return
-	  "registrazioneutente"; }
-	  
-	  LocalDate dataDiNascita = utente.getProfilo().getDataDiNascita(); if
-	  (dataDiNascita != null) { LocalDate oggi = LocalDate.now(); Period period =
-	  Period.between(dataDiNascita, oggi); if (period.getYears() < 18) {
-	  model.addAttribute("erroreMaggiorenne", true); return "registrazioneutente";
-	  } }
-	  
-	  // Altre operazioni di registrazione
-	    utenteService.registraUtente(utente);
-
-    @GetMapping
-    public String getPage(Model model) {
-        Utente utente = new Utente();
-        model.addAttribute("utente", utente);
-        return "registrazioneutente";
-    }
-
     @PostMapping
     public String processaFormRegistrazione(@ModelAttribute("utente") Utente utente,
                                              BindingResult result, Model model) {
